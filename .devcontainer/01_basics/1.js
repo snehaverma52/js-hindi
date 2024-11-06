a797b31e3fee79e334056f6584f5895dac060838
snehaverma52 <snehakiran19446@gmail.com>
2024-11-06 23:00:18 +0530

    data types and variables

---
+++ .devcontainer/01_basics/1.js
@@ -0,0 +1,21 @@
+const accountId = "19446"
+let accountEmail = "snehakiran19446@gmail.com"
+var accountPassword = "password123"
+accountCity= "Bangalore"
+let accountState ;
+ /*const = change na hoga ya nhi kar skta hai// let = change kar skta hai// var = change kar skta hai*/
+
+//accountId =2 //not allowed
+
+// js ma scope hota hai usma scope ka problem hota tha....block scope pa iska control nhi tha bilukul bhi{ya curly braces
+//  hi scope ha bro} bhul jao --------- var bhi kuch ha use karoo ----- let or const 
+// var ka use nhi karna let or const karo bhyii
+// prefer not to use var because of issue in block scope and function scope
+
+
+ accountEmail = "shfrgjg@gmail.com"
+ accountPassword = "35644"
+ accountCity = "Mumbai"
+
+ console.log(accountId);
+ console.table([accountId , accountPassword , accountCity , accountEmail , accountState]); 
---
+++ .devcontainer/01_basics/2.js
@@ -0,0 +1,28 @@
+
+"use strict"; // This enables strict mode, which helps catch common coding errors and "unsafe" actions
+
+// alert("Hello, world!"); // This is an alert dialog box that displays the message "Hello, world!"
+// alert(3+3)  // we are using nodejs not browser This is an alert dialog box that displays the result of the expression 3+3, which is 6 
+
+console.log("Hello, world!"); 
+console.log(3 + 3); 
+
+let name = "sneha"
+let age =19
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigInt => for larger numbers
+// string => " " or ' '
+// boolean => true or false
+// null => null ....standalone value === it is a represatation of empty value
+// undefined => undefined ....it means a variable has been declared but not assigned a value
+// symbol => unique value
+
+
+
+// object => key value pairs
+
+console.log(typeof null) // its output is object for null case only
+console.log(typeof undefined) // its output is undefined
